2b1f11034c56ce07663f32677613f7a2ffdc5399
RestrictedTM <tylermiddleton60@gmail.com>
2018-09-12 15:52:01 -0700

    finalDraft[0]

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -12,30 +12,93 @@
 // array to hold random value to choose for random number
 
 var random_result;
-var win;
-var loss;
+var win = 0;
+var lost = 0;
+var previous = 0;
+ 
 
 $(document).ready(function() {
 
-random_result = Math.floor(Math.random() *  120) + 19;
 
-console.log(random_result);
 
-$("#result").html('Random Result: ');
+//result isnt using min
+var resetAndStart = function function_name(arguement){
     
-for(var i = 0; i < 4; i++){
+    $(".crystals").empty();
+
+    var images = [
+    'https://i.pinimg.com/originals/6e/91/46/6e9146efb8487faff07a36dc41f68bfa.gif',
+    'https://68.media.tumblr.com/44cb3e3115039f4167440687f92aca45/tumblr_ofgsdj5fvk1vsjcxvo1_540.gif',
+    'https://hbimg.b0.upaiyun.com/2eb639dcd451a97d1d5ff5273a3f4774af8914ba2f4f7f-fqMXhJ',
+    'https://i.giphy.com/Z06UFnGuY0H9C.gif'];
+
+    random_result = Math.floor(Math.random() * 102 ) + 19;
+
+    $("#result").html('Random Result: ' + random_result);
+        
+    for(var i = 0; i < 4; i++){
+
+        
+        var random = Math.floor(Math.random() * 11) + 1;
 
     
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
-    var crystal = $("<div>");
-        crystal.attr({
-            "class": 'crystal',
-            "date-random": random
-        });
-    
-        $(".crystals").append(crystal);
+        var crystal = $("<div>");
+            crystal.attr({
+                "class": 'crystal',
+                "data-random": random
+            });
+            crystal.css({
+                "background-image": "url('" + images[i] + "')",
+                "background-size":"cover"
+            });
 
+            crystal.html(random);
+        
+            $(".crystals").append(crystal); 
+            
+
+    }
+    $("#previous").html("Current Total: " + previous);
 }
+
+resetAndStart();
+
+//document listens to the dom instead of button then button is declared in argument
+$(document).on('click', ".crystal", function() {
+    
+    //turns the var number into a number value from a string
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+    $("#previous").html("Current Total: " + previous);
+
+    console.log(previous);
+
+    if(previous > random_result){
+       
+        lost++;
+       
+       $("#lost").html("You lost: " + lost);
+
+       previous = 0;
+
+       resetAndStart();
+       
+    }
+    else if(previous === random_result){
+        
+        win++;
+
+        $("#win").html("You won: " + win);
+
+        previous= 0;
+
+        resetAndStart();
+    }
+   
+
+});
+
 });
 
